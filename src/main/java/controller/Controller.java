a565069ff1b0b94cffee0700840ba0e1332b191e
serebryakkov <serebryakkov@gmail.com>
2020-04-01 17:45:10 +0300

    T

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -13,8 +13,10 @@ import java.util.Map;
 public class Controller {
     private UserService userService = new UserService();
     private HelpTextService helpTextService = new HelpTextService();
+    private HostService hostService;
 
     private StartMessage startMessage = new StartMessage();
+    private HostsListMessage hostsListMessage;
 
     /**
      * Метод добавляет юзера в БД если его там нет
@@ -28,10 +30,10 @@ public class Controller {
         startMessage.sendMessage(user, text, bot);
     }
 
-//    public void getAndSendHostsList(User user, Bot bot) {
-//        hostService = new HostService();
-//        Map<Integer, String> hostsList = hostService.getAllByUserId(user);
-//        String text = helpTextService.getByCode("hosts_list");
-//        message.sendMessage(user, text, hostsList, bot);
-//    }
+    public void getAndSendHostsList(User user, Bot bot) {
+        hostService = new HostService();
+        Map<Integer, String> hostsList = hostService.getAllByUserId(user);
+        String text = helpTextService.getByCode("hosts_list");
+        hostsListMessage.sendMessage(user, text, hostsList, bot);
+    }
 }
