09425af0b84a1a3e99b9958924f374ca1c150063
serebryakkov <serebryakkov@gmail.com>
2020-04-01 17:13:02 +0300

    T

--- src/main/java/server/Bot.java
+++ src/main/java/server/Bot.java
@@ -23,11 +23,6 @@ public class Bot extends TelegramLongPollingBot {
                     user.setId(message.getChatId());
                     user.setUsername(message.getChat().getUserName());
                     controller.startBot(user, this);
-                } else if (text.equals("Список сайтов")) {
-                    User user = new User();
-                    user.setId(message.getChatId());
-                    user.setUsername(message.getChat().getUserName());
-                    controller.getAndSendHostsList(user, this);
                 }
             }
         } else if (update.hasCallbackQuery()) {
