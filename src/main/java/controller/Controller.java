14b3eda6484d7841e5898647a2e9f3a4142cad30
serebryakkov <serebryakkov@gmail.com>
2020-04-16 19:45:51 +0300

    T

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -10,11 +10,6 @@ import ui.*;
 import java.util.List;
 
 public class Controller {
-    private Message message = new Message();
-    private HostsListMessage hostsListMessage = new HostsListMessage();
-    private HostNameRequestMessage hostNameRequestMessage = new HostNameRequestMessage();
-    private HostInfoMessage hostInfoMessage = new HostInfoMessage();
-
     /**
      * Метод добавляет юзера в БД если его там нет
      * и отправляет ему стартовое сообщение.
@@ -24,7 +19,7 @@ public class Controller {
     public void startBot(User user, Bot bot) {
         User.add(user);
         String text = HelpText.getByCode("welcome");
-        message.sendMessage(user, text, bot);
+        new Message().sendMessage(user, text, bot);
     }
 
     /**
@@ -36,24 +31,24 @@ public class Controller {
         List<Host> hosts = Host.getAllByUserId(user);
         user.setHosts(hosts);
         String text = HelpText.getByCode("hosts_list");
-        hostsListMessage.sendMessage(user, text, bot);
+        new HostsListMessage().sendMessage(user, text, bot);
     }
 
     public void getAndSendAboutBotInfo(User user, Bot bot) {
         String text = HelpText.getByCode("about_bot");
-        message.sendMessage(user, text, bot);
+        new Message().sendMessage(user, text, bot);
     }
 
     public void sendHostNameRequest(User user, Bot bot) {
         User.updateUserState(user);
         String text = HelpText.getByCode("enter_host_name");
-        hostNameRequestMessage.sendMessage(user, text, bot);
+        new HostNameRequestMessage().sendMessage(user, text, bot);
     }
 
     public void cancelHostAdding(User user, Bot bot) {
         User.updateUserState(user);
         String text = HelpText.getByCode("site_adding_cancel");
-        message.sendMessage(user, text, bot);
+        new Message().sendMessage(user, text, bot);
         getAndSendHostsList(user, bot);
     }
 
@@ -67,24 +62,24 @@ public class Controller {
             user.setState(User.State.NULL);
             User.updateUserState(user);
             String text = HelpText.getByCode("host_successfully_added");
-            message.sendMessage(user, text, bot);
+            new Message().sendMessage(user, text, bot);
             getAndSendHostsList(user, bot);
         } else {
             String text = HelpText.getByCode("invalid_url");
-            new Message().sendMessage(user, text, bot);
+            new HostNameRequestMessage().sendMessage(user, text, bot);
         }
     }
 
     public void getAndSendHostInfo(User user, String hostId, Bot bot) {
         Host host = Host.getById(Integer.parseInt(hostId));
-        hostInfoMessage.sendMessage(user, host, bot);
+        new HostInfoMessage().sendMessage(user, host, bot);
     }
 
     public void deleteHostAndSendMessage(User user, String hostId, Bot bot) {
         Host.remove(user, Integer.parseInt(hostId));
         Monitor.stopAndRemoveMonitor(Host.getById(Integer.parseInt(hostId)));
         String text = HelpText.getByCode("host_successfully_deleted");
-        message.sendMessage(user, text, bot);
+        new Message().sendMessage(user, text, bot);
         getAndSendHostsList(user, bot);
     }
 }
