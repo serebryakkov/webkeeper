6908163f0308002d0a62e7f4f810d6657e07c3a9
serebryakkov <serebryakkov@gmail.com>
2020-05-28 15:07:28 +0300

    T

--- src/main/java/dao/IDAOFactoryImpl.java
+++ src/main/java/dao/IDAOFactoryImpl.java
@@ -2,6 +2,7 @@ package dao;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,11 +10,16 @@ public class IDAOFactoryImpl implements IDAOFactory {
     private static final ComboPooledDataSource pool = new ComboPooledDataSource();
 
     static {
-        pool.setMinPoolSize(1);
-        pool.setMaxPoolSize(18);
+        try {
+            pool.setDriverClass("org.postgresql.Driver");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
         pool.setJdbcUrl(System.getenv("DB_URL"));
         pool.setUser(System.getenv("DB_USER"));
         pool.setPassword(System.getenv("DB_PASSWORD"));
+        pool.setMinPoolSize(1);
+        pool.setMaxPoolSize(18);
     }
 
     @Override
