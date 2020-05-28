201756c055f972fc84453745fbb3ba2db707192e
serebryakkov <serebryakkov@gmail.com>
2020-05-28 14:49:44 +0300

    T

--- src/main/java/dao/IDAOFactoryImpl.java
+++ src/main/java/dao/IDAOFactoryImpl.java
@@ -9,11 +9,11 @@ public class IDAOFactoryImpl implements IDAOFactory {
     private static final ComboPooledDataSource pool = new ComboPooledDataSource();
 
     static {
+        pool.setMinPoolSize(1);
+        pool.setMaxPoolSize(18);
         pool.setJdbcUrl(System.getenv("DB_URL"));
         pool.setUser(System.getenv("DB_USER"));
         pool.setPassword(System.getenv("DB_PASSWORD"));
-        pool.setMinPoolSize(1);
-        pool.setMaxPoolSize(18);
     }
 
     @Override
