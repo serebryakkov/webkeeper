5b696e1f0c42cd4ffec1c83935d727571f28cc5f
serebryakkov <serebryakkov@gmail.com>
2020-05-29 16:28:37 +0300

    T

--- src/main/java/dao/IDAOFactoryImpl.java
+++ src/main/java/dao/IDAOFactoryImpl.java
@@ -1,13 +1,10 @@
 package dao;
 
 import org.apache.commons.dbcp2.*;
-import org.apache.commons.pool2.ObjectPool;
-import org.apache.commons.pool2.impl.GenericObjectPool;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.concurrent.TimeUnit;
 
 public class IDAOFactoryImpl implements IDAOFactory {
 
@@ -21,27 +18,9 @@ public class IDAOFactoryImpl implements IDAOFactory {
         dataSource.setUsername(System.getenv("DB_USER"));
         dataSource.setPassword(System.getenv("DB_PASSWORD"));
         dataSource.setMaxIdle(18);
+        dataSource.setInitialSize(1);
 
         ds = dataSource;
-
-//        DriverManagerConnectionFactory connectionFactory =
-//                new DriverManagerConnectionFactory(
-//                        System.getenv("DB_URL"),
-//                        System.getenv("DB_USER"),
-//                        System.getenv("DB_PASSWORD")
-//                );
-//
-//        PoolableConnectionFactory poolableConnectionFactory =
-//                new PoolableConnectionFactory(
-//                        connectionFactory,
-//                        null);
-//
-//        poolableConnectionFactory.setMaxConnLifetimeMillis(TimeUnit.MINUTES.toMillis(1));
-//
-//        ObjectPool<PoolableConnection> connectionPool =
-//                new GenericObjectPool<>(poolableConnectionFactory);
-//
-//        dataSource = new PoolingDataSource<>(connectionPool);
     }
 
     public static IDAOFactoryImpl getInstance() {
