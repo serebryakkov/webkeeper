a173dd52dc4a446750d74c4d215e2a108cde70eb
serebryakkov <serebryakkov@gmail.com>
2020-05-31 15:46:29 +0300

    T

--- src/main/java/bot/hostkeeper/server/Bot.java
+++ src/main/java/bot/hostkeeper/server/Bot.java
@@ -15,8 +15,6 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-@Component
-@ImplementedBy(org.telegram.telegrambots.meta.generics.BotSession.class)
 public class Bot extends TelegramLongPollingBot {
     private final Controller controller = new Controller();
 
