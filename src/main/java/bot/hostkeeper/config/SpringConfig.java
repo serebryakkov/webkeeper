5cde5dad0905a2a5bc9373d9993b73ffee8535dc
serebryakkov <serebryakkov@gmail.com>
2020-05-31 15:22:44 +0300

    T

--- src/main/java/bot/hostkeeper/config/SpringConfig.java
+++ src/main/java/bot/hostkeeper/config/SpringConfig.java
@@ -1,9 +1,16 @@
 package bot.hostkeeper.config;
 
+import bot.hostkeeper.server.Bot;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("bot/hostkeeper/")
 public class SpringConfig {
+
+    @Bean
+    public Bot getBot() {
+        return new Bot();
+    }
 }
